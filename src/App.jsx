ba1b9eae8b05375f154826c43890e39c518c46ba
Ravin Samudika <raveensamudika@gmail.com>
2024-10-12 12:34:29 +0530

    Complete nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
+import AppHeader from "./layout/AppHeader";
+
+
 function App() {
-
-
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <AppHeader/>
     </>
   );
 }
